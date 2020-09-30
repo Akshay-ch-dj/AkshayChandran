7629c4e49c5ef48e843e9978415f274ea31bc8c3
Akshay-ch-dj <akshaych203@gmail.com>
2020-09-30 23:44:30 +0530

    Added navbar transition effect.
    smooth scrolling & snap effect added.

--- portfolio_page.js
+++ portfolio_page.js
@@ -11,3 +11,26 @@ function addBottomBar(e) {
 
     e.target.parentNode.classList.add('active');
 }
+
+// Navbar transformation with intersection observer
+
+const navContainer = document.querySelector(".nav-home");
+
+const sectionHome = document.getElementById("welcome");
+
+const homeOptions = {
+    rootMargin: "-200px 0px 0px 0px"
+};
+
+const homeObserver = new IntersectionObserver(function(entries, homeObserver) {
+    entries.forEach(entry => {
+        // console.log(entry.target);
+        if (!entry.isIntersecting) {
+            navContainer.classList.add('nav-container');
+        } else {
+            navContainer.classList.remove('nav-container');
+        }
+    });
+}, homeOptions);
+
+homeObserver.observe(sectionHome);
